438ece3a5aec882e0e0e28d4ecc4099213e2b323
puppybala <puppybala@icloud.com>
2023-08-19 02:12:43 +0800

    quickSort

---
+++ quickSort.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func quickSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	pivot := arr[0]
+	var less []int
+	var greater []int
+	for _, v := range arr[1:] {
+		if v <= pivot {
+			less = append(less, v)
+		} else {
+			greater = append(greater, v)
+		}
+	}
+	return append(append(quickSort(less), pivot), quickSort(greater)...)
+}
+func main() {
+	fmt.Println("Hello, World!")
+	arr := []int{10, 5, 2, 3}
+	fmt.Println(arr)
+	fmt.Println(quickSort(arr))
+}
